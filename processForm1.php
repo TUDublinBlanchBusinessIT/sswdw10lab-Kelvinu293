8855dd4c47aaae396578691503a54f65b83a2346
Kelvin Uzo <b00141682@mytudublin.ie>
2024-11-19 15:28:32 +0000

    Fix luggage number

--- processForm1.php
+++ processForm1.php
@@ -4,7 +4,7 @@ session_start();
 $_SESSION['passengerFN'] = $_POST['firstname'];
 $_SESSION['passengerSN'] = $_POST['surname'];
 if (isset($_POST['luggage'])){
-    $_SESSION['luggage'] = 0;
+    $_SESSION['luggage'] = 1;
     header('location: luggage.html');
 }
 else{
